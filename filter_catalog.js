544307b587b7d92699d9232cc083bc1599b71373
Peab2003 <149892005+Peab2003@users.noreply.github.com>
2023-12-30 15:51:14 +0700

    Add files via upload

---
+++ filter_catalog.js
@@ -0,0 +1,31 @@
+// Lấy phần tử select và thêm sự kiện onchange để xử lý khi giá trị thay đổi
+document.getElementById("select").onchange = function() {
+    const selectValue = this.value;
+    const list = document.querySelector('.list');
+    const items = list.querySelectorAll('.item');
+
+    // Chuyển NodeList thành mảng để có thể sử dụng sort
+    const itemsArray = Array.from(items);
+
+    if (selectValue === 'LowToHigh') {
+        itemsArray.sort((a, b) => {
+            const priceA = parseFloat(a.querySelector('.price').innerText.replace(/\D/g, ''));
+            const priceB = parseFloat(b.querySelector('.price').innerText.replace(/\D/g, ''));
+            return priceA - priceB;
+        });
+    } else if (selectValue === 'HighToLow') {
+        itemsArray.sort((a, b) => {
+            const priceA = parseFloat(a.querySelector('.price').innerText.replace(/\D/g, ''));
+            const priceB = parseFloat(b.querySelector('.price').innerText.replace(/\D/g, ''));
+            return priceB - priceA;
+        });
+    }
+
+    // Xóa các phần tử hiện có trong list
+    list.innerHTML = '';
+
+    // Thêm lại các phần tử đã sắp xếp vào list
+    itemsArray.forEach(item => {
+        list.appendChild(item);
+    });
+};
