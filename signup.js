188a9713c9ee8d74f49ec7c438daed941e9b1d7a
Peab2003 <149892005+Peab2003@users.noreply.github.com>
2023-12-31 23:19:47 +0700

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,38 @@
+// Hàm kiểm tra mật khẩu
+function validatePassword() {
+  let password1 = document.getElementById("password1").value;
+  let password2 = document.getElementById("password2").value;
+
+  if (password1 !== password2) {
+      document.getElementById("password2").setCustomValidity("Mật khẩu không khớp");
+  } else {
+      document.getElementById("password2").setCustomValidity('');
+  }
+}
+
+// Bắt sự kiện cho việc kiểm tra mật khẩu khi nhập
+document.getElementById('password1').addEventListener('input', validatePassword);
+document.getElementById('password2').addEventListener('input', validatePassword);
+
+// Xử lý khi form được submit
+let loginForm = document.querySelector(".my-form");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let email = document.getElementById("email");
+  let password = document.getElementById("password1"); // Sử dụng ID đúng
+
+  // Mật khẩu xác nhận là trường password2
+  let confirmPassword = document.getElementById("password2"); // Sử dụng ID đúng
+
+  if (password.value !== confirmPassword.value) {
+      window.alert('Mật khẩu xác nhận không khớp. Vui lòng kiểm tra lại!');
+  } else {
+      console.log("Email:", email.value);
+      console.log("Mật khẩu:", password.value);
+      console.log("Xác nhận mật khẩu:", confirmPassword.value);
+      // Hiển thị thông báo thành công và chuyển hướng đến index.html
+      window.alert('Đăng ký thành công!');
+      window.location.href = 'index.html';
+  }
+});
