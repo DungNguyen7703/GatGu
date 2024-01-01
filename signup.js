9356adb29f01a64d82b990f98e1fb3966aea85a4
Peab2003 <149892005+Peab2003@users.noreply.github.com>
2024-01-02 00:50:47 +0700

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,19 +1,3 @@
-// Hàm kiểm tra mật khẩu
-function validatePassword() {
-  let password1 = document.getElementById("password1").value;
-  let password2 = document.getElementById("password2").value;
-
-  if (password1 !== password2) {
-      document.getElementById("password2").setCustomValidity("Mật khẩu không khớp");
-  } else {
-      document.getElementById("password2").setCustomValidity('');
-  }
-}
-
-// Bắt sự kiện cho việc kiểm tra mật khẩu khi nhập
-document.getElementById('password1').addEventListener('input', validatePassword);
-document.getElementById('password2').addEventListener('input', validatePassword);
-
 // Xử lý khi form được submit
 let loginForm = document.querySelector(".my-form");
 
